9c2389097efc6eefd37a3638aa58c9173665ba4f
Sebastian Ortiz <sebastian_ortiz94@yahoo.com>
2020-08-24 10:38:11 -0300

    .

---
+++ Laboratorio de Computación I/TP 3/ej13.cpp
@@ -0,0 +1,39 @@
+/*
+
+13) Dada una lista de 7 números informar cual es el primer y último número impar
+ingresado.
+Ejemplo 8, 4, -5, 6, 9, 5, 18 se informa -5 y 5
+
+*/
+
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main()
+{
+
+int num, pImp = 0, uImp;
+
+for(int i=0;i<5;i++)
+
+{
+    cout << "Ingrese un numero: " << endl;
+    cin >> num;
+
+    if(num%2!=0 && pImp==0)
+    {
+        pImp = num;
+    }
+    else if(num%2!=0)
+    {
+        uImp = num;
+    }
+}
+
+    cout << "Primer impar: " << pImp << endl;
+    cout << "Ultimo impar: " << uImp << endl;
+
+  system("pause");
+  return 0;
+}
